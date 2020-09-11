fb0ed34176e64f0a662e5c726e2875be374ae721
MatiMonaco <matimonaco@outlook.com>
2020-09-11 12:49:17 -0300

    Update

--- LogicAnd.py
+++ LogicAnd.py
@@ -8,18 +8,44 @@ expected_output = np.array(([-1, -1, -1, 1]))
 
 and_perceptron = PerceptronSimple(2)
 
-alpha = 0.5
+alpha = 0.0025
 threshold = 0
-limit = 100
-
+limit = int(1 / alpha)
+error = 0
+min_error = entries_len * 2
+min_weight = np.zeros(entries_len)
 weights_history = [and_perceptron.weights]
 print(and_perceptron.weights)
-for i in range(0, limit):
+i = 0
+count = 0
+while min_error > 0 and i in range(0, limit):
+    error = 0
+    if count > 1000 * entries_len:
+        and_perceptron.weights = 2 * np.random.random_sample(2) - 1
+        count = 0
+
     for j in range(0, entries_len):
+
         and_perceptron.propagation(entries[j, 0:2], threshold)
         and_perceptron.update(alpha, expected_output[j])
         weights_history = np.concatenate((weights_history, [and_perceptron.weights]))
+        error += 1 if (and_perceptron.output != expected_output[j]) else 0
+    print("error: ", error)
+    if error < min_error:
+        min_error = error
+        min_weight = and_perceptron.weights.copy()
+    i += 1
+    count += 1
+
+
+and_perceptron.weights = min_weight
+for i in range(0, entries_len):
+    and_perceptron.propagation(entries[i, 0:2], threshold)
+    print("Entry: ", entries[i, 0:2])
+    print("Expected Output: ",expected_output[i])
+    print("Output: ", and_perceptron.output)
 
 plt.plot(weights_history[:, 0], 'k')
 plt.plot(weights_history[:, 1], 'r')
 plt.show()
+print(weights_history)
--- PerceptronSimple.py
+++ PerceptronSimple.py
@@ -5,7 +5,7 @@ class PerceptronSimple:
     def __init__(self, n):
         self.n = n
 
-        self.weights = np.random.random_sample(2)*2-1
+        self.weights = np.random.random_sample(2) * 2 - 1
 
         self.output = None
         self.entries = None
@@ -15,11 +15,6 @@ class PerceptronSimple:
         self.entries = entries
 
     def update(self, alpha, expected_output):
-        print("antes: ",self.weights)
-        print("expected_output: ",expected_output)
-        print("output: ",self.output)
         for i in range(0, self.n):
-            print("entries[",i,"]: ",self.entries[i])
-            self.weights[i] = self.weights[i] + alpha * (expected_output - self.output) * self.entries[i]
 
-        print("updated: ", self.weights)
+            self.weights[i] = self.weights[i] + alpha * (expected_output - self.output) * self.entries[i]
